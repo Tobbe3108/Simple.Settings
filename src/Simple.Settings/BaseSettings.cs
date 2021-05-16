145618247c6bb3f2ed0555dc3680e1cf71e84196
Tobbe3108 <Tobbe3108@gmail.com>
2021-05-17 01:52:37 +0200

    BaseSettings class

---
+++ src/Simple.Settings/BaseSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.Settings
+{
+  public abstract class BaseSettings
+  {
+    public abstract FileInfo FileInfo { get; protected set; }
+    public abstract void Load(string path, bool createIfNotExisting = true);
+
+    public abstract Task LoadAsync(string path, bool createIfNotExisting = true);
+
+    public abstract void Save();
+
+    public abstract Task SaveAsync();
+  }
+}
