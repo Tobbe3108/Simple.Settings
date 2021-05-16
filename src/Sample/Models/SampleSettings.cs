36b4aa00ef37903aa2a55f4ec0790ff0b6b05071
Tobbe3108 <Tobbe3108@gmail.com>
2021-05-17 01:53:30 +0200

    Sample project

---
+++ src/Sample/Models/SampleSettings.cs
@@ -0,0 +1,9 @@
+﻿using Simple.Settings.Json;
+
+namespace Sample.Models
+{
+  public class SampleSettings : Settings
+  {
+    public string SomeSetting { get; set; } = "Test";
+  }
+}
---
+++ src/Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Sample.Models;
+
+namespace Sample
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      var path = Path.Combine(Directory.GetCurrentDirectory(), "Test.json");
+      
+      var setting = new SampleSettings();
+
+      setting.Load(path);
+
+      setting.SomeSetting = "1";
+      
+      setting.Save();
+
+      await setting.LoadAsync(path);
+
+      setting.SomeSetting = "TestEdited";
+      
+      await setting.SaveAsync();
+    }
+  }
+}
