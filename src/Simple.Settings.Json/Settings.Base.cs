96d97b119958cf7288ecb7ad113784a864008b38
Tobbe3108 <Tobbe3108@gmail.com>
2021-05-19 01:12:09 +0200

    Nullable fixes

--- src/Simple.Settings.Json/Settings.Base.cs
+++ src/Simple.Settings.Json/Settings.Base.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.Json;
@@ -10,20 +11,22 @@ namespace Simple.Settings.Json
   {
     [JsonIgnore] public override FileInfo FileInfo { get; protected set; }
 
-    [JsonIgnore] private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions
+    // ReSharper disable once MemberCanBePrivate.Global
+    // ReSharper disable once FieldCanBeMadeReadOnly.Global
+    [JsonIgnore] public JsonSerializerOptions JsonSerializerOptions = new()
     {
       WriteIndented = true
     };
     
     private static void CopyValues<T>(T target, T source)
     {
-      var properties = target.GetType().GetProperties(
+      var properties = target?.GetType().GetProperties(
         BindingFlags.Instance | BindingFlags.Public).Where(prop =>
         prop.CanRead
         && prop.CanWrite
         && prop.GetIndexParameters().Length == 0);
 
-      foreach (var prop in properties)
+      foreach (var prop in properties ?? Array.Empty<PropertyInfo>())
       {
         if (prop.Name == nameof(FileInfo))
         {
